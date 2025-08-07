d4b9a00187454ff442376a177a1c7f680da71f9d
Neiland <rodrigo.naohan@gmail.com>
2025-08-07 07:51:04 -0300

    Fibonacci

---
+++ ProgramacaoDinamica/Fibonacci.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+#define MAX 999
+
+// int Fibanacci(int n){
+
+//     return 0;
+// }
+
+
+int main(){
+
+    int n = 4, F[MAX];
+
+    for (int i = 0; i < MAX; i++){
+        F[i] = -1;
+    }
+
+    for (int i = 0; i < MAX; i++){
+        cout << F[i] << endl;
+    }
+
+}
